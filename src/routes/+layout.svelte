e96dd5ebdbe371d545a7d423195591ef5c8957bf
Christofer <imchris@tuta.io>
2023-09-15 20:52:47 +0400

    remove quotes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,7 @@
 	import './styles.css'
 	import '@fontsource-variable/work-sans'
 	import '@fontsource/ibm-plex-mono/500.css'
-	import { quotes } from '$lib/components/Quotes'
 	import { getRandom } from '$lib/Helper.mjs'
-
-	onMount(() => {
-		if (import.meta.env.PROD) {
-			console.log(getRandom(quotes))
-		}
-	})
 </script>
 
 <Navbar />
