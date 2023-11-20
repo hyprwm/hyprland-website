6c44b38bdda5e4cba677678d596c66344ae54b81
VDawg <vdawg@tuta.io>
2023-11-20 19:26:35 +0400

    HoF: Use data file to generate rices

--- src/routes/hall_of_fame/+page.svelte
+++ src/routes/hall_of_fame/+page.svelte
@@ -2,7 +2,8 @@
 	import Title from '$lib/components/Title.svelte'
 	import Contest from './Contest.svelte'
 	import FamedRice from './FamedRice.svelte'
-	import colors from 'tailwindcss/colors'
+
+	export let data
 </script>
 
 <section>
@@ -22,103 +23,20 @@
 		</Title>
 	</div>
 
-	<Contest name="Space" number={3} date="November 2023" --color={colors.indigo[500]}>
-		<FamedRice
-			name="Celestial"
-			creator="Flafy"
-			pretitel="#1"
-			dotfilesLink="https://github.com/flafydev/nixos-config/"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/44374434?s=96&v=4"
-			thumbnail="/imgs/ricing_competitions/3/flafy.webp"
-		/>
-		<FamedRice
-			name="Globes"
-			creator="Aylur"
-			pretitel="#2"
-			dotfilesLink="https://github.com/Aylur/dotfiles"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/104676705?v=4"
-			thumbnail="/imgs/ricing_competitions/3/aylur.webp"
-		/>
-		<FamedRice
-			name="Golden Era"
-			creator="VDawg"
-			pretitel="#3"
-			dotfilesLink="https://github.com/Visual-Dawg/space_dots"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/28539403?s=96&v=4"
-			thumbnail="/imgs/ricing_competitions/3/vdawg.webp"
-		/>
-	</Contest>
-
-	<Contest name="Summer" number={2} date="May 2023" --color="orange">
-		<FamedRice
-			name="Summer Gruv"
-			creator="end_4"
-			pretitel="#1"
-			dotfilesLink="https://github.com/end-4/dots-hyprland/tree/summer-gruv"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/97237370?s=24&v=4"
-			thumbnail="/imgs/ricing_competitions/2/end_4.webp"
-		/>
-		<FamedRice
-			name="Unnamed"
-			creator="Flafy"
-			pretitel="#2"
-			dotfilesLink="https://github.com/FlafyDev/flutter_background_bar"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/44374434?s=96&v=4"
-			thumbnail="/imgs/ricing_competitions/2/flafy.webp"
-		/>
-		<FamedRice
-			name="Day and Night"
-			creator="Mathisbuilder"
-			pretitel="#3"
-			dotfilesLink="https://github.com/MathisP75/summer-day-and-night"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/98901170?v=4"
-			thumbnail="/imgs/ricing_competitions/2/day-night.webp"
-		/>
-	</Contest>
-
-	<Contest name="Winter" number={1} date="December 2022" --color={colors.cyan[500]}>
-		<FamedRice
-			name="Unnamed"
-			creator="Flafy"
-			pretitel="#1"
-			dotfilesLink="https://github.com/FlafyDev/flutter_workspaces_2"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/25975326?v=4"
-			thumbnail="/imgs/ricing_competitions/1/flafy.webp"
-		/>
-		<FamedRice
-			name="Aurora"
-			creator="flick0"
-			pretitel="#2 (ex aequo)"
-			dotfilesLink="https://github.com/flick0/dotfiles/tree/aurora"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/77581181?s=48&v=4"
-			thumbnail="/imgs/ricing_competitions/1/flicko.webp"
-		/>
-		<FamedRice
-			name="Hyprland Winter"
-			creator="amadeus"
-			pretitel="#2 (ex aequo)"
-			dotfilesLink="https://github.com/AmadeusWM/hyprland-winter"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/63149896?s=48&v=4"
-			thumbnail="/imgs/ricing_competitions/1/amadeus.webp"
-		/>
-		<FamedRice
-			name="Unnamed"
-			creator="Lyasm"
-			pretitel="#3 (ex aequo)"
-			dotfilesLink="#"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/111616244?s=48&v=4"
-			thumbnail="/imgs/ricing_competitions/1/lyasm.webp"
-		/>
-		<FamedRice
-			name="Unnamed"
-			creator="lauroro"
-			pretitel="#3 (ex aequo)"
-			dotfilesLink="https://github.com/lauroro/hyprland-dotfiles"
-			creatorProfilePicture="https://avatars.githubusercontent.com/u/88981092?s=48&v=4"
-			thumbnail="/imgs/ricing_competitions/1/lauroro.webp"
-			class="pb-16"
-		/>
-	</Contest>
+	{#each data.contests as { name, date, number, color, rices }}
+		<Contest {name} {date} {number} --color={color}>
+			{#each rices as { name: riceName, creator, creatorProfilePicture, dotfilesLink, pretitel, thumbnail }}
+				<FamedRice
+					name={riceName}
+					{creator}
+					{creatorProfilePicture}
+					{pretitel}
+					{dotfilesLink}
+					{thumbnail}
+				/>
+			{/each}
+		</Contest>
+	{/each}
 </section>
 
 <svelte:head>
