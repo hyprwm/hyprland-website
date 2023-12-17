2729abc5dac6a5d06d8dacdf4125bf51cde866ba
VDawg <vdawg@tuta.io>
2023-12-17 21:59:22 +0100

    newsSlice: adjust gradient

--- src/routes/NewsSlice.svelte
+++ src/routes/NewsSlice.svelte
@@ -5,7 +5,7 @@
 	export let news
 </script>
 
-<section class="relative max-w-5xl px-8">
+<section class="relative mb-12 max-w-5xl px-8 md:mb-20">
 	<Title class="mb-8">
 		<span slot="pre">Read while it's fresh</span>
 		<span slot="title">Latest news</span>
@@ -22,6 +22,12 @@
 
 <style lang="postcss">
 	.light {
+		width: 1800px;
+		min-height: 500px;
+		height: 220%;
+		translate: -30% 0%;
+		/* margin-top: -200px; */
+
 		background: url('/imgs/grain.webp'),
 			radial-gradient(
 				50% 50% at 50% 50%,
@@ -30,17 +36,14 @@
 				transparent
 			);
 
-		mask-image: radial-gradient(
-			100% 80% at top,
+		/* mask-image: radial-gradient(
+			50% 50% at 50% 50%,
 			white,
 			rgba(0, 0, 0, 1),
 			rgba(0, 0, 0, 1),
 			transparent
-		);
+		); */
 
-		width: 1200px;
-		height: 800px;
-		margin-left: -400px;
 		position: absolute;
 		z-index: -10;
 		top: 0;
