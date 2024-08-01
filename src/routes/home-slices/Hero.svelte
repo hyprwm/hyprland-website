36b1d58391cbcc00aef8980f1ad31a3fcf857015
VDawg <vdawg@tuta.io>
2024-08-02 01:08:57 +0300

    improve hero design

--- src/routes/home-slices/Hero.svelte
+++ src/routes/home-slices/Hero.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <section
-	class="relative flex h-[100svh] min-h-[52rem] w-full flex-col items-center justify-center overflow-x-hidden"
+	class="relative flex h-[100svh] min-h-[36rem] w-full flex-col items-center justify-center overflow-x-hidden"
 	use:inview
 	on:inview_change={({ detail: { inView } }) => {
 		isVisible = inView
@@ -27,31 +27,26 @@
 	<div
 		class="pointer-events-none z-10 flex h-full min-h-max flex-col items-center justify-center px-5"
 	>
-		<div
-			class="ani-in -mt-10 mb-8 flex flex-col items-center gap-6 text-center fill-mode-backwards sm:-mt-20"
-		>
-			<HyprlandLogo class=" mb-6 h-40 w-48   sm:h-48 sm:w-56" />
-		</div>
 		<h1
-			class="ani-in title pointer-events-auto mb-4 max-w-[20ch] text-center !leading-[1.25] fill-mode-backwards [animation-delay:384ms]"
+			class="ani-in title pointer-events-auto mb-4 max-w-[20ch] text-center text-4xl font-bold !leading-[1.25] fill-mode-backwards [animation-delay:384ms] sm:text-6xl md:text-7xl lg:text-8xl lg:tracking-tight"
 		>
 			Tiling compositor <br /><span class="hyprgradient title-gradient">with the looks</span>
 		</h1>
 		<p
-			class="ani-in mb-8 text-center text-xl font-medium text-slate-300 fill-mode-backwards [animation-delay:794ms] sm:max-w-lg sm:px-0 md:max-w-lg"
+			class="ani-in mb-8 text-center text-base font-medium text-slate-400 fill-mode-backwards [animation-delay:794ms] sm:mb-12 sm:max-w-lg sm:px-0 sm:text-xl md:max-w-lg md:text-2xl lg:max-w-[50ch]"
 		>
 			Hyprland provides the latest Wayland features, dynamic tiling, all the eyecandy, powerful
-			plugins and much more.
+			plugins and much more
 		</p>
 
 		<div
 			class="pointer-events-auto flex items-center gap-7 duration-500 animate-in fade-in-0 slide-in-from-bottom-4 fill-mode-backwards [animation-delay:1390ms]"
 		>
 			<a href="https://wiki.hyprland.org/Getting-Started/Installation/">
-				<Button size="lg">Install</Button>
+				<Button size="xl">Install</Button>
 			</a>
 			<a href="https://wiki.hyprland.org/">
-				<Button type="fancyOutline" size="lg">Wiki</Button>
+				<Button type="fancyOutline" size="xl">Wiki</Button>
 			</a>
 		</div>
 	</div>
@@ -72,7 +67,6 @@
 			theme(colors.cyan.500 / 0%)
 		);
 		background-clip: text;
-		@apply text-4xl font-bold sm:text-5xl md:text-7xl;
 	}
 
 	.title-gradient {
