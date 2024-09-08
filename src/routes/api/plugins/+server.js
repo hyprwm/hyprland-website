4795b5d761a6260d58d1858a22f4bee4c5e139fb
VDawg <vdawg@tuta.io>
2024-09-08 20:11:28 +0300

    Small stuff (#69)
    * refactor Title
    
    refactor title component
    
    * featureSlice: nicer max-width
    
    * add meta title to news
    
    * plugins as toml, no plugin sub-pages
    
    fixup toml
    
    * plugins: better letter if no icon present
    
    * add more plugins to showcase
    
    * fix blur script

--- src/routes/api/plugins/+server.js
+++ src/routes/api/plugins/+server.js
@@ -2,23 +2,16 @@ import { json } from '@sveltejs/kit'
 import path from 'path'
 import { parse } from 'smol-toml'
 import { readFile } from 'node:fs/promises'
+import pluginsToml from '../../../content/plugins.toml?raw'
 
-/** Get the plugins of the `content/plugins/` directory */
 async function getPlugins() {
-	const plugins = await readFile(
-		path.join(import.meta.dirname, '../../../content/plugins.toml'),
-		'utf-8'
+	const plugins = parse(pluginsToml).plugins.sort(
+		(a, b) =>
+			(b.featured ?? 0) - (a.featured ?? 0) ||
+			(b.weight ?? 0) - (a.weight ?? 0) ||
+			((b.logo && 1) ?? 0) - ((a.logo && 1) ?? 0) ||
+			((b.banner && 1) ?? 0) - ((a.banner && 1) ?? 0)
 	)
-		.then(parse)
-		.then(({ plugins }) =>
-			plugins.sort(
-				(a, b) =>
-					(b.featured ?? 0) - (a.featured ?? 0) ||
-					(b.weight ?? 0) - (a.weight ?? 0) ||
-					((b.logo && 1) ?? 0) - ((a.logo && 1) ?? 0) ||
-					((b.banner && 1) ?? 0) - ((a.banner && 1) ?? 0)
-			)
-		)
 
 	return plugins
 }
